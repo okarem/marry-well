abfa8b7276a31a0231d0d7c8e6a6536cca6336df
hadeel11 <faour.hadeel@gmail.com>
2020-03-11 15:01:17 +0200

    backend checks user if exists in the json file, and inserts user,password to json file. relates#5

--- server/src/controllers/createUser.js
+++ server/src/controllers/createUser.js
@@ -1,15 +1,31 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const {createUser} = require("../models/users");
 
-exports.createUser = (req, res) => {
-    console.log(req.body);
-    const data = req.body;
-    const userName = data.username;
-    const password = data.password;
-    const confirmedPassword = data.confirmedPassword;
-    
-    console.log(userName);
-    console.log(password);
 
-    res.send('hi');
-}
+
+exports.addUser = (req, res) => {
+  console.log(req.body);
+  const data = req.body;
+  const userName = data.username;
+  const password = data.password;
+  const confirmedPassword = data.confirmedPassword;
+
+
+
+  if (password === confirmedPassword) {
+
+       bcrypt
+      .hash(password, 10)
+      .then(hash => {
+        createUser(userName,hash)
+        .then(message=>{console.log(message)})
+        .catch(errMessage=>{console.log(errMessage)});
+      })
+      .catch(err => console.log(err));
+  }else{
+      res.send("password and confirmed password are not equal");
+     
+  }
+  res.redirect("http://localhost:3000/");
+};
