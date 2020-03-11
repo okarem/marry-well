e8ccb023929d6d03e5c03e0a9c3a06c9173f57db
Fatmeeh <lasheenfatmeeh@gmail.com>
2020-03-11 12:38:51 +0200

    guests It will be displayed as a form that the user fills. gives the user the ability to add and show the guests-list.A feature that lets the user manage his/her list via grouping by families.Home page button.

--- client/src/screens/Guests/Guests.jsx
+++ client/src/screens/Guests/Guests.jsx
@@ -4,45 +4,48 @@ import './Guests.css';
 const Guests = () => {
    return (
       <div>
-            <div className='header'>
+         <div className='header'>
             <div className='My-Guests'>
                <h2> قائمة المعازيم</h2>
                <div className="icon">
-             
-            <img className="categoryImage" src="./img/guests.png" alt="guests"></img>
-           <a href='/'>
-           <img className="icon-Home" src="./img/Home.png" alt="Home"></img>
-             </a>
-             
-             </div>
-             </div>
-            
+
+                  <img className="categoryImage" src="./img/guests.png" alt="guests"></img>
+                  <a href='/'>
+                     <img className="icon-Home" src="./img/Home.png" alt="Home"></img>
+                  </a>
+
+               </div>
             </div>
-           <div className='form'>
+
+         </div>
+         <div className='form'>
             <div className='Add-Guests'>
                <input type="text"></input>
-               <button className="Guests-Button"> اضافه</button>
+               <select id="status">
+                  <option value="single">اعزب</option>
+                  <option value="family">عائلته</option>
+                  <option value="relationship"> خطيبته</option>
+               </select>
+               <select id="FemaleMale">
+                  <option value="female">انثى </option>
+                  <option value="male">ذكر</option>
+               </select>
+               <button className="Guests-Button"> اضافه</button><div className='select'>
+
+               </div>
             </div>
-            
+
             <div>
+
                <ul className='list-Guests'>
-                  <li> فاطمه </li>    
-                  <li>محمود</li>
-                  <li>هديل</li>
-                 
+                  <div className='Name'><li> فاطمه </li><button className="Delet-Button">🗑️</button></div>
+                  <div className='Name'><li>محمود</li><button className="Delet-Button">🗑️</button></div>
+                  <div className='Name'><li>هديل</li><button className="Delet-Button">🗑️</button></div>
                </ul>
             </div>
 
          </div>
       </div>
-
    );
 };
-
-{/* <ul className='list-Guests'>
-<div className='Name'><li> فاطمه </li><button className="Delet-Button">🗑️</button></div>
-<div className='Name'><li>محمود</li><button className="Delet-Button">🗑️</button></div>
-<div className='Name'><li>هديل</li><button className="Delet-Button">🗑️</button></div>
-</ul> */}
-
 export default Guests;
