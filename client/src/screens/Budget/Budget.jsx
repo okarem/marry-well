209f1774cc38dd0d2b1c47e93d6ef3898a993ecd
Fatmeeh <lasheenfatmeeh@gmail.com>
2020-03-12 12:23:03 +0200

    add bar to budget

--- client/src/screens/Budget/Budget.jsx
+++ client/src/screens/Budget/Budget.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import MaterialTable from 'material-table';
-import axios from 'axios';
+import React from "react";
+import MaterialTable from "material-table";
+import axios from "axios";
+import ProgressBar from '../progressBar/progressBar'
 
-import './Budget.css';
-import CircularDeterminate from '../../components/Progress/Progress';
+import "./Budget.css";
+import CircularDeterminate from "../../components/Progress/Progress";
 
 const Budget = () => {
+  const  [progressBarTitle]  = React.useState('تكاليف')
+  const [progressBarImage]=React.useState('./img/cost.png')
+
   const [budgetDataState, setBudgetDataState] = React.useState({
     columns: [
-      { title: 'المجموعة', field: 'category', lookup: { 1: 'اغراض', 2: 'عاملين' } },
-      { title: 'السعر', field: 'price', type: 'currency' },
-      { title: 'الكمية', field: 'quantity', type: 'numeric' },
-      { title: 'اسم الغرض', field: 'itemName' }
+      {
+        title: "المجموعة",
+        field: "category",
+        lookup: { 1: "اغراض", 2: "عاملين" }
+      },
+      { title: "السعر", field: "price", type: "currency" },
+      { title: "الكمية", field: "quantity", type: "numeric" },
+      { title: "اسم الغرض", field: "itemName" }
     ]
   });
 
   React.useEffect(() => {
     axios
-      .get('http://localhost:5000/api/getBudget')
+      .get("http://localhost:5000/api/getBudget")
       .then(res => res.data)
       .then(finalRes => {
         setBudgetDataState({ ...budgetDataState, data: finalRes });
@@ -28,27 +36,34 @@ const Budget = () => {
   if (budgetDataState.data === undefined) {
     return <CircularDeterminate />;
   }
-
+  
   return (
+    <div>
+    <ProgressBar progressBarTitle={progressBarTitle}  progressBarImage={progressBarImage} />
+
     <div className="budgetTable">
       <MaterialTable
         title=""
         localization={{
           header: {
-            actions: ''
+            actions: ""
           },
-          toolbar: { searchTooltip: 'بحث', searchPlaceholder: ' بحث', exportTitle: 'csv تحميل ملف ' },
+          toolbar: {
+            searchTooltip: "بحث",
+            searchPlaceholder: " بحث",
+            exportTitle: "csv تحميل ملف "
+          },
 
           body: {
-            emptyDataSourceMessage: 'لا يوجد معطيات',
-            addTooltip: 'اضافة',
-            deleteTooltip: 'حذف',
-            editTooltip: 'تعديل',
+            emptyDataSourceMessage: "لا يوجد معطيات",
+            addTooltip: "اضافة",
+            deleteTooltip: "حذف",
+            editTooltip: "تعديل",
 
             editRow: {
-              saveTooltip: 'تأكيد',
-              cancelTooltip: 'الغاء',
-              deleteText: 'هل انت متأكد من حذف هذا السطر؟'
+              saveTooltip: "تأكيد",
+              cancelTooltip: "الغاء",
+              deleteText: "هل انت متأكد من حذف هذا السطر؟"
             }
           }
         }}
@@ -58,19 +73,25 @@ const Budget = () => {
           showTitle: false,
           paging: false,
           actionsColumnIndex: -1,
-          searchFieldAlignment: 'right',
+          searchFieldAlignment: "right",
           exportButton: true,
-          rowStyle: { backgroundColor: '#EEF0F2', color: '#353B3C' },
-          searchFieldStyle: { direction: 'rtl' },
-          headerStyle: { textAlign: 'center', backgroundColor: '#353B3C', color: '#C6C7C4', fontSize: '20px', fontWeight: 'bold' },
-          cellStyle: { textAlign: 'center', fontSize: '16px' },
-          padding: 'dense'
+          rowStyle: { backgroundColor: "#EEF0F2", color: "#353B3C" },
+          searchFieldStyle: { direction: "rtl" },
+          headerStyle: {
+            textAlign: "center",
+            backgroundColor: "#353B3C",
+            color: "#C6C7C4",
+            fontSize: "20px",
+            fontWeight: "bold"
+          },
+          cellStyle: { textAlign: "center", fontSize: "16px" },
+          padding: "dense"
         }}
         editable={{
           onRowAdd: newData =>
             new Promise(resolve => {
               axios
-                .post('http://localhost:5000/api/addBudgetItem', { newData })
+                .post("http://localhost:5000/api/addBudgetItem", { newData })
                 .then(res => console.log(res))
                 .catch(err => console.log(err));
 
@@ -110,6 +131,8 @@ const Budget = () => {
         }}
       />
     </div>
+    </div>
+
   );
 };
 
