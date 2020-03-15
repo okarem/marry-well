fb521ffb78ffa39d1048ddc4c331343fad17659d
Fatmeeh <lasheenfatmeeh@gmail.com>
2020-03-15 16:06:29 +0200

    stuff jsdon index.js stuff.js

--- client/src/screens/Stuff/Stuff.jsx
+++ client/src/screens/Stuff/Stuff.jsx
@@ -1,69 +1,101 @@
-import React from 'react';
-import MaterialTable from 'material-table';
-import axios from 'axios';
-import ProgressBar from '../../components/progressBar/progressBar';
-import './Stuff.css';
-
+import React from "react";
+import MaterialTable from "material-table";
+import axios from "axios";
+import ProgressBar from "../../components/progressBar/progressBar";
+import "./Stuff.css";
+import CircularDeterminate from "../../components/Progress/Progress";
 
 const Stuff = () => {
-  const [progressBarTitle] = React.useState('الاغراض');
-  const [progressBarImage] = React.useState('./img/shopping-bags.png');
+  const [progressBarTitle] = React.useState("الاغراض");
+  const [progressBarImage] = React.useState("./img/shopping-bags.png");
 
-  const [stuffData, setStuffData] = React.useState({
+  const [stuffDataState, setStuffDataState] = React.useState({
     columns: [
-      { title: 'المجموعة', field: 'category', lookup: { 1: 'البيت', 2: 'الحفله' } },
-      { title: 'اسم الغرض', field: 'itemName' }
+      {
+        title: "المجموعة",
+        field: "category",
+        lookup: { 1: "البيت", 2: "الحفله" }
+      },
+      { title: "اسم الغرض", field: "itemName" }
     ],
-    data: [{ itemName: 'فناجين', category: 1 }]
+    data: [{ itemName: "فناجين", category: 1 }]
   });
+
+  React.useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/getStuff")
+      .then(res => res.data)
+      .then(finalRes => {
+        setStuffDataState({ ...stuffDataState, data: finalRes });
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  if (stuffDataState.data === undefined) {
+    return <CircularDeterminate />;
+  }
 
   return (
     <div>
       <div>
-        <ProgressBar progressBarTitle={progressBarTitle} progressBarImage={progressBarImage} />
+        <ProgressBar
+          progressBarTitle={progressBarTitle}
+          progressBarImage={progressBarImage}
+        />
       </div>
       <div className="table">
         <MaterialTable
           title="تكاليف المستلزمات"
           localization={{
             header: {
-              actions: ''
+              actions: ""
             },
-            toolbar: { searchTooltip: 'بحث', searchPlaceholder: 'بحث' },
+            toolbar: { searchTooltip: "بحث", searchPlaceholder: "بحث" },
 
             body: {
-              emptyDataSourceMessage: 'لا يوجد معطيات',
-              addTooltip: 'اضافة',
-              deleteTooltip: 'حذف',
-              editTooltip: 'تعديل',
+              emptyDataSourceMessage: "لا يوجد معطيات",
+              addTooltip: "اضافة",
+              deleteTooltip: "حذف",
+              editTooltip: "تعديل",
 
               editRow: {
-                saveTooltip: 'تأكيد',
-                cancelTooltip: 'الغاء',
-                deleteText: 'هل انت متأكد من حذف هذا السطر؟'
+                saveTooltip: "تأكيد",
+                cancelTooltip: "الغاء",
+                deleteText: "هل انت متأكد من حذف هذا السطر؟"
               }
             }
           }}
-          columns={stuffData.columns}
-          data={stuffData.data}
+          columns={stuffDataState.columns}
+          data={stuffDataState.data}
           options={{
             showTitle: false,
             paging: false,
             actionsColumnIndex: -1,
-            searchFieldAlignment: 'right',
+            searchFieldAlignment: "right",
             exportButton: true,
-            rowStyle: { backgroundColor: '#EEF0F2', color: '#353B3C' },
-            searchFieldStyle: { direction: 'rtl' },
-            headerStyle: { textAlign: 'center', backgroundColor: '#353B3C', color: '#C6C7C4', fontSize: '20px', fontWeight: 'bold' },
-            cellStyle: { textAlign: 'center', fontSize: '16px' },
-            padding: 'dense'
+            rowStyle: { backgroundColor: "#EEF0F2", color: "#353B3C" },
+            searchFieldStyle: { direction: "rtl" },
+            headerStyle: {
+              textAlign: "center",
+              backgroundColor: "#353B3C",
+              color: "#C6C7C4",
+              fontSize: "20px",
+              fontWeight: "bold"
+            },
+            cellStyle: { textAlign: "center", fontSize: "16px" },
+            padding: "dense"
           }}
           editable={{
             onRowAdd: newData =>
               new Promise(resolve => {
+                axios
+                  .post('http://localhost:5000/api/addBudgetItem', { newData })
+                  .then(res => console.log(res))
+                  .catch(err => console.log(err));
+
                 setTimeout(() => {
                   resolve();
-                  setStuffData(prevStuffData => {
+                  setStuffDataState(prevStuffData => {
                     const data = [...prevStuffData.data];
                     data.push(newData);
                     return { ...prevStuffData, data };
@@ -75,7 +107,7 @@ const Stuff = () => {
                 setTimeout(() => {
                   resolve();
                   if (oldData) {
-                    setStuffData(prevStuffData => {
+                    setStuffDataState(prevStuffData => {
                       const data = [...prevStuffData.data];
                       data[data.indexOf(oldData)] = newData;
                       return { ...prevStuffData, data };
@@ -87,7 +119,7 @@ const Stuff = () => {
               new Promise(resolve => {
                 setTimeout(() => {
                   resolve();
-                  setStuffData(prevStuffData => {
+                  setStuffDataState(prevStuffData => {
                     const data = [...prevStuffData.data];
                     data.splice(data.indexOf(oldData), 1);
                     return { ...prevStuffData, data };
