7c8311fb1dbb84c2e2bb287be1e58af91319a8d4
Fatmeeh <lasheenfatmeeh@gmail.com>
2020-03-17 14:41:59 +0200

    DElet and Update Stuff Table rel

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const user = require('./users');
 const { fetchBudgetData, addBudgetDataItem } = require('./budget');
-const { fetchStuffData, addStuffDataItem } = require('./stuff');
+const { fetchStuffData, addStuffDataItem, updateStuffDataItem, deleteStuffDataItem } = require('./stuff');
 const { addGuestsData } = require('./guests');
 
 const createUser = require('./createUser');
@@ -12,6 +12,9 @@ router.get('/api/getBudget', fetchBudgetData);
 router.post('/api/addBudgetItem', addBudgetDataItem);
 router.get('/api/getStuff', fetchStuffData);
 router.post('/api/addStuffItem', addStuffDataItem);
+router.put('/api/updateStuffItem', updateStuffDataItem);
+router.delete('/api/deleteStuffItem', deleteStuffDataItem);
+
 router.post('/authenticate', user.authenticateUser);
 router.post('/createUser', createUser.addUser);
 router.post('/api/addGuests', addGuestsData);
