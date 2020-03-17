5227b73701cad5aa42bc3573404597093aeff1be
hadeel11 <faour.hadeel@gmail.com>
2020-03-17 11:52:25 +0200

    Adds guests data to the guests table, frontend and backend relates #60

--- server/src/controllers/guests.js
+++ server/src/controllers/guests.js
@@ -3,7 +3,6 @@ const { addGuests } = require('../models/guests');
 
 
 exports.addGuestsData = (req, res) => {
-   console.log(req.body.newData);
   const { name, city, gender, status } = req.body.newData;
   addGuests(name, city, gender, status,(err, result) => {
     if (err) return err;
--- server/src/models/guests.js
+++ server/src/models/guests.js
@@ -2,7 +2,7 @@ const dbConnection = require('../database/db_connection');
 
 
     exports.addGuests = (name, city, gender, status,cb) => {
-        userId=1;
+        userId=3;
         dbConnection.query(
             'INSERT INTO guests (userId,name, city, gender, status) VALUES ($1,$2,$3,$4,$5)',
              [userId, name, city, gender, status],
