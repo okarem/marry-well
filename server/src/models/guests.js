1b095494c341a644cbdd6df068b082230a9f98c2
hadeel11 <faour.hadeel@gmail.com>
2020-03-19 20:47:18 +0200

    fixes all the credentials issues, And fixes all the not working queries

--- server/src/models/guests.js
+++ server/src/models/guests.js
@@ -3,28 +3,33 @@ const dbConnection = require('../database/db_connection');
 exports.getGuestsData = (id, cb) => {
   dbConnection.query('SELECT * FROM guests where userid = $1', [id], cb);
 };
+
 exports.addGuestsItem = (userId, name, city, gender, status, cb) => {
   {
-    dbConnection.query('INSERT INTO items (userId,name, city, gender, status) VALUES ($1,$2,$3,$4,$5)', [userId, name, city, gender, status], err => {
-      if (err) return cb(err);
-      return cb(null, 'New Item Added');
-    });
+    dbConnection.query(
+      'INSERT INTO guests (userId,name, city, gender, status) VALUES ($1,$2,$3,$4,$5)',
+      [userId, name, city, gender, status],
+      err => {
+        if (err) return cb(err);
+        return cb(null, 'New Guest Added');
+      }
+    );
   }
 };
-exports.updateGuestsItem = (guestsid, name, city, gender, status, cb) => {
+exports.updateGuestsItem = (guestid, name, city, gender, status, cb) => {
   dbConnection.query(
-    'UPDATE guests SET name = $1, city = $2 ,gender=$3,status=$4, WHERE guestsid = $5 ',
-    [name, city, gender, status, guestsid],
+    'UPDATE guests SET name = $1, city = $2 ,gender=$3,status=$4 WHERE guestid = $5 ',
+    [name, city, gender, status, guestid],
     err => {
       if (err) return cb(err);
-      return cb(null, 'Item Updated');
+      return cb(null, 'Guest Updated');
     }
   );
 };
 
-exports.deleteGuestsItem = (guestsid, cb) => {
-  dbConnection.query('DELETE FROM guests WHERE guestsid = $1', [guestsid], err => {
+exports.deleteGuestsItem = (guestid, cb) => {
+  dbConnection.query('DELETE FROM guests WHERE guestid = $1', [guestid], err => {
     if (err) return cb(err);
-    return cb(null, 'Item has been deleted');
+    return cb(null, 'Guest has been deleted');
   });
 };
